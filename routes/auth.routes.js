d59e0c43062c391b6e3676daf1235c00d23c41c1
BFTorres <bfonseca@gmx.de>
2021-05-13 12:03:01 +0200

    login auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const UserModel = require("../models/User.model");
 
+
+//signup
 router.post('/signup', (req, res) => {
   const {username, email, password } = req.body;
   console.log(username, email, password);
@@ -28,3 +30,43 @@ router.post('/signup', (req, res) => {
         }
       })
 });
+
+//login
+router.post('/login', (req, res) => {
+  const {email, password} = req.body;
+  UserModel.findOne({email})
+  .then((userData) => {
+       //check if passwords match
+      bcrypt.compare(password, userData.passwordHash)
+        .then((doesItMatch) => {
+            //if it matches
+            if (doesItMatch) {
+              // req.session is the special object that is available to you
+              userData.passwordHash = "***";
+              req.session.loggedInUser = userData;
+              res.status(200).json(userData)
+            }
+            //if passwords do not match
+            else {
+                res.status(500).json({
+                    error: 'Passwords don\'t match',
+                })
+              return; 
+            }
+        })
+        .catch(() => {
+            res.status(500).json({
+                error: 'Email format not correct',
+            })
+          return; 
+        });
+  })
+  .catch((err) => {
+    res.status(500).json({
+        error: 'Email does not exist',
+        message: err
+    })
+    return;  
+  });
+})
+
